224f1fd1e0d607dcce4e673074931d53a93bcffd
igazka <31811932+igazka@users.noreply.github.com>
2021-05-14 22:34:21 +0200

    localOrg_works, video still missing

--- localFolderOrg.py
+++ localFolderOrg.py
@@ -3,11 +3,14 @@ import os
 import glob
 import exif
 from exif import Image
+from os import listdir
+from os.path import isfile, join
 
 destination="E:\-=My_shit=-\google_backup\Takeout\\all"
 path ="E:\\-=My_shit=-\\google_backup\\Takeout\\all"
 images_path=[]
 images_data=[]
+videos_path=[]
 for root, dirs, files in os.walk(path):
     for file in files:
         if file.endswith(".jpg"):
@@ -20,11 +23,36 @@ for root, dirs, files in os.walk(path):
              #newpath=shutil.copy(str(os.path.join(root, file)), destination)
              #os.remove(os.path.join(root, file))
              images_path.append(str(os.path.join(root, file)))
+        elif file.endswith(".mp4"):
+             #print(os.path.join(root, file))
+             #newpath=shutil.copy(str(os.path.join(root, file)), destination)
+             #os.remove(os.path.join(root, file))
+             videos_path.append(str(os.path.join(root, file)))
+        elif file.endswith(".3gp"):
+             #print(os.path.join(root, file))
+             #newpath=shutil.copy(str(os.path.join(root, file)), destination)
+             #os.remove(os.path.join(root, file))
+             videos_path.append(str(os.path.join(root, file)))             
 
+
+onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
 for index, image in enumerate(images_path):
     with open(image, "rb") as phfile:
         phs = Image(phfile)
-        if phs.datetime_original!="":
-            print(phs.datetime_original)
-        
+        #if phs.datetime_original!="":
+        #    print(phs.datetime_original)
+        attr=phs.list_all()
+        for item in attr:
+            if item=="datetime_original":
+                print(str(index)+"/"+str(len(onlyfiles))+" Oragnized")
+                year=phs.datetime_original[:4]
+                month=phs.datetime_original[5:7]
+                destination=os.path.join(path, year+"_"+month)
+                if not os.path.isdir(destination):
+                    os.mkdir(destination)
+                if not os.path.isfile(os.path.join(image, destination)):
+                    newpath=shutil.copy(image, destination)
+    os.remove(image)
+    print(str(index)+"/"+str(len(onlyfiles))+" Deleted")
+
--- main.py
+++ main.py
@@ -5,11 +5,12 @@ List libraries including owned, shared and group libraries.
 
 import  requests
 
+#'repos/182038b5-958f-49ee-8d68-ba58fec3b346/file/?p=/portfolio.Igaz_portfolio.xml'
 # replace with your token
 token = 'Authorization: Token f63b5aaca9574f1054879efc2cf43fb9b9dc02d1'
-url = 'http://192.168.1.132:9208/api2/ping'
-format='Accept: application/json; indent=4' 
-payload=token
+url = 'http://192.168.1.132:9208/api2/repos/182038b5-958f-49ee-8d68-ba58fec3b346/file/?p=/portfolio.Igaz_portfolio.xml'
+format='Accept: application/json; charset=utf-8; indent=4'
+payload={token,format}
 response = requests.get(url,params=payload)
 
 print(response.text)
