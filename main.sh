2bf4f4369bba355f8211b2bd2787b4ff581c6a51
igazka <31811932+igazka@users.noreply.github.com>
2021-05-19 11:53:55 +0200

    process indicator added

--- main.sh
+++ main.sh
@@ -20,10 +20,10 @@ repo="182038b5-958f-49ee-8d68-ba58fec3b346"
     orgDirfiles=($orgDirfiles)
     echo "--------------------------------------------"
     echo "num of files: ${#orgDirfiles[@]}"
-#for file in "${orgDirfiles[@]}"
-    #do
-    file=${orgDirfiles[1]} #remove this before changing to for loop
-    #echo $file
+for file in "${!orgDirfiles[@]}";do
+    #file=${orgDirfiles[1]} #remove this before changing to for loop
+    echo "Processing: $file of ${#orgDirfiles[@]}"
+    printf '${orgDirfiles[%s]}=%s\n' "$file" "${orgDirfiles[file]}"
     #check if file is photo
     #        if [[ "$file" == *".jpg"* ]]; then
     #            echo "It's a jpg."
@@ -31,28 +31,31 @@ repo="182038b5-958f-49ee-8d68-ba58fec3b346"
     #            echo "It's a mp4."
     #        fi
     #download to temp
-        echo "First to process: ${file}"
-        if test -f "$file"; then
-            echo "$file exists."
+        if test -f "${orgDirfiles[file]}"; then
+            echo "${orgDirfiles[file]} exists."
         else
-            link=$(curl -H "Authorization: Token ${token}" -H 'Accept: application/json; charset=utf-8; indent=4' ${url}"api2/repos/182038b5-958f-49ee-8d68-ba58fec3b346/file/?p=/${orgDir}/${file}&reuse=0" | jq --raw-output '.')
+            link=$(curl -H "Authorization: Token ${token}" -H 'Accept: application/json; charset=utf-8; indent=4' ${url}"api2/repos/182038b5-958f-49ee-8d68-ba58fec3b346/file/?p=/${orgDir}/${orgDirfiles[file]}&reuse=0" | jq --raw-output '.')
             echo "$link"
             curl -O "${link}"
         fi
     #get exif data
-        if [[ "$file" == *".jpg"* ]]; then
-           date=$(exiftool -p '$dateTimeOriginal' "$file")
-            echo "$date"
-            year=${date::4}
-            month=${date:5:2}            
-        elif [[ "$file" == *".mp4"* ]]; then
-            echo "It's a mp4."
+        if [[ "${orgDirfiles[file]}" == *".jpg"* ]]; then
+           date=$(exiftool -p '$dateTimeOriginal' "${orgDirfiles[file]}")
+        #needs case if exif date is not found!!!  
+        elif [[ "${orgDirfiles[file]}" == *".mp4"* ]]; then
+            date=$(exiftool -p '$mediacreatedate' "${orgDirfiles[file]}")
         fi
+        year=${date::4}
+        month=${date:5:2}  
         echo "year: $year"
         echo "month: $month"
         destSubDir="${year}"_"${month}"
+        echo $destSubDir
+        if  "$destSubDir"="_"; then
+            destSubDir="NoDate"
+        fi
     #delete file
-        #rm "$file"
+        rm "${orgDirfiles[file]}"
     #check if directory exists
         destDirContent=$(curl -H "Authorization: Token ${token}" -H 'Accept: application/json; indent=4' ${url}api2/repos/${repo}/dir/?p=/${destDir} | jq --raw-output '.[] | {name} | .[]')
         destDirContent=($destDirContent)
@@ -63,8 +66,8 @@ repo="182038b5-958f-49ee-8d68-ba58fec3b346"
         if [[ ! " ${destDirContent[@]} " =~ " ${destSubDir} " ]]; then
             # whatever you want to do when array doesn't contain value
             #create Dir
-            curl -d "operation=mkdir" -H "Authorization: Token ${token}" -H 'Accept: application/json; charset=utf-8; indent=4' ${url}api2/repos/${repo}/dir/?p=/${destDir}/${destSubDir}
+            curl -s -d "operation=mkdir" -H "Authorization: Token ${token}" -H 'Accept: application/json; charset=utf-8; indent=4' ${url}api2/repos/${repo}/dir/?p=/${destDir}/${destSubDir}
         fi
     #move file on srv
-        curl -v -d "operation=move&dst_repo=${repo}&dst_dir=/${destDir}/${destSubDir}" -H "Authorization: Token ${token}" -H 'Accept: application/json; charset=utf-8; indent=4' ${url}api2/repos/${repo}/file/?p=/${orgDir}/${file}
-    #done
+        curl -s -d "operation=move&dst_repo=${repo}&dst_dir=/${destDir}/${destSubDir}" -H "Authorization: Token ${token}" -H 'Accept: application/json; charset=utf-8; indent=4' ${url}api2/repos/${repo}/file/?p=/${orgDir}/${orgDirfiles[file]}
+done
