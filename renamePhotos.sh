487acba7d190ee3b0863871a5c4e9aacd55006f2
igazka <31811932+igazka@users.noreply.github.com>
2021-06-20 14:32:12 +0200

    if noexifdata, handled, also if duplacate, name then numbered

--- renamePhotos.sh
+++ renamePhotos.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #bash script to rename photos basesed on their EXIF data IMG_YYYYMMDD_HHMMSS.jpg
+#go to working folder    
+    orgDir=/home/andras/terraswinyo/images
+    cd $orgDir
 # list of files from folder
-    orgDirfiles=$(ls -p /home/andras/terraswinyo/images | grep -v /)
+    orgDirfiles=$(ls -p | grep -v /)
     IFS=$'\n' orgDirfiles=($orgDirfiles)
     echo "--------------------------------------------"
     echo "num of files: ${#orgDirfiles[@]}"
-    exit 0
 for file in "${!orgDirfiles[@]}";do
     echo -e "Processing: \e[1;31m$file\e[0m  of ${#orgDirfiles[@]}" 
     echo "${orgDirfiles[file]}"
@@ -21,22 +23,30 @@ for file in "${!orgDirfiles[@]}";do
         if [[ "${orgDirfiles[file]}" == *".jpg"* || "${orgDirfiles[file]}" == *".JPG"* ]]; then
            date=$(exiftool -p '$dateTimeOriginal' "${orgDirfiles[file]}")
            ext=".jpg"
+           prefix="IMG"
         elif [[ "${orgDirfiles[file]}" == *".mp4"* || "${orgDirfiles[file]}" == *".MP4"* ]]; then
            date=$(exiftool -p '$mediacreatedate' "${orgDirfiles[file]}")
            ext=".mp4"
+           prefix="VID"
         elif  [[ "${orgDirfiles[file]}" == *".3gp"* || "${orgDirfiles[file]}" == *".3GP"* ]]; then
             date=$(exiftool -p '$mediacreatedate' "${orgDirfiles[file]}")
             ext=".3gp"
+            prefix="VID"
         fi
-        year=${date::4}
-        month=${date:5:2}
-        day=${date:8:2}
-        hour=${date:11:2}
-        minutes=${date:14:2}
-        seconds=${date:17:2}  
-        newFilename="IMG_"$year$month$day"_"$hour$minutes$seconds$ext
-        echo "New filename: $newFilename"
+        #if there is no exif data, rename to NOEXIF_DATA + current name
+        if [[ "${#date}" -gt 0 ]]; then
+            year=${date::4}
+            month=${date:5:2}
+            day=${date:8:2}
+            hour=${date:11:2}
+            minutes=${date:14:2}
+            seconds=${date:17:2}  
+            newFilename=$prefix"_"$year$month$day"_"$hour$minutes$seconds$ext
+            echo "New filename: $newFilename"
+        else
+            newFilename=$prefix"_NOEXIF_DATA"${orgDirfiles[file]}$ext
+        fi
     #renaming
-        #mv ${orgDirfiles[file]} $newFilename
+        mv --backup=numbered ${orgDirfiles[file]} $newFilename
         echo "File: ${orgDirfiles[file]} is renamed to $newFilename."
 done
