75771d3057e2ce8a277ed9d693042d133cc2949a
igazka <igazka100@gmail.com>
2021-07-27 19:32:33 +0000

    rename added

--- unixLocal/photos_renameOrg.sh
+++ unixLocal/photos_renameOrg.sh
@@ -16,47 +16,50 @@ srcFolder2=/home/andras/terraswinyo/images/CameraBogi
         else
             echo "num of new files: ${#orgDirfiles[@]}"
                 for file in "${!orgDirfiles[@]}";do
-                    checkFileName ${orgDirfiles[file]}
+                    checkFileExt ${orgDirfiles[file]}
+                    if [[ $? -eq 1 ]]; then
+                    continue
+                    else
+                    getExif ${orgDirfiles[file]}
+                    rename  ${orgDirfiles[file]}
+                    fi
                 done
         fi
     }
-    checkFileName(){ #check if filename is valid
+    checkFileExt(){ #check if fileextension is valid
         length=$(expr length "$1")
         if [[ $length -gt 0 ]]&&[[ "${1: -4}" == ".jpg" || "${1: -4}" == ".JPG" || "${1: -4}" == ".3gp" || "${1: -4}" == ".mp4" ]]; then
-            echo "good filename: $1"
+            echo "good fileExtension: $1"
+            return 0
         else
             echo "bad file extension: $1, skipping"
+            return 1
         fi
     }
-workDirContentlist $srcFolder1
-#workDirContentlist $srcFolder2        
-exit 0
-
-    #get exif data
-    getExif () {
-        if [[ "${orgDirfiles[file]}" == *".jpg"* || "${orgDirfiles[file]}" == *".JPG"* ]]; then
-           date=$(exiftool -p '$dateTimeOriginal' "${orgDirfiles[file]}" -F)
+    getExif () { #get exif data
+        if [[ "${1: -4}" == ".jpg" || "${1: -4}" == ".JPG" ]]; then
+           date=$(exiftool -p '$dateTimeOriginal' "$1" -F)
            if [[ "${#date}" -eq 0 || "$date" == "0000:00:00 00:00:00" ]]; then
-             date=$(exiftool -p '$DateAcquired' "${orgDirfiles[file]}" -F)
+             date=$(exiftool -p '$DateAcquired' "$1" -F)
              if [[ "${#date}" -eq 0 || "$date" == "0000:00:00 00:00:00" ]]; then
-              date=$(exiftool -p '${FileModifyDate#;DateFmt("%Y:%m:%d %H:%M:%S")}' "${orgDirfiles[file]}")
+              date=$(exiftool -p '${FileModifyDate#;DateFmt("%Y:%m:%d %H:%M:%S")}' "$1")
              fi
            fi
            ext=".jpg"
            prefix="IMG"
-         elif [[ "${orgDirfiles[file]}" == *".mp4"* || "${orgDirfiles[file]}" == *".MP4"* ]]; then
-           date=$(exiftool -p '$mediacreatedate' "${orgDirfiles[file]}" -F)
+         elif [[ "${1: -4}" == ".mp4" || "${1: -4}" == ".MP4" ]]; then
+           date=$(exiftool -p '$mediacreatedate' "$1" -F)
            ext=".mp4"
            prefix="VID"
-         elif  [[ "${orgDirfiles[file]}" == *".3gp"* || "${orgDirfiles[file]}" == *".3GP"* ]]; then
-            date=$(exiftool -p '$mediacreatedate' "${orgDirfiles[file]}" -F)
+         elif  [[ "${1: -4}" == ".3gp" || "${1: -4}" == ".3GP" ]]; then
+            date=$(exiftool -p '$mediacreatedate' "$1" -F)
             ext=".3gp"
             prefix="VID"
         fi
-        echo $date, "${#date}"
+        #echo $date, "${#date}"
         #if there is no exif data, rename to NOEXIF_DATA + current name
         if [[ "${#date}" -eq 0 || "$date" == "0000:00:00 00:00:00" ]]; then
-            newFilename=$prefix"_NOEXIF_DATA_"${orgDirfiles[file]}$ext
+            newFilename=$prefix"_NOEXIF_DATA_"$1$ext
             destSubDir="NoDate"
         else
             year=${date::4}
@@ -68,17 +71,20 @@ exit 0
             newFilename=$prefix"_"$year$month$day"_"$hour$minutes$seconds$ext
             destSubDir="${year}"/"${month}"
         fi
+        #echo "$newFilename $destSubDir"
     }
-
-    #rename
-    rename () {
-             if [[ "${orgDirfiles[file]}" == "$newFilename" ]]; then
-            # echo "File ${orgDirfiles[file]} is \e[1;32malready\e[0m named $newFilename." 
+  rename () {
+        if [[ "$1" == "$newFilename" ]]; then
+             echo "File $1 is \e[1;32malready\e[0m named $newFilename." 
            else 
-             mv --backup=numbered ${orgDirfiles[file]} $newFilename
-            # echo "\e[1;32mFile: ${orgDirfiles[file]} is renamed to $newFilename.\e[0m"
+             mv --backup=numbered $1 $newFilename
+             echo "\e[1;32mFile: $1 is renamed to $newFilename.\e[0m"
         fi 
     }
+workDirContentlist $srcFolder1
+#workDirContentlist $srcFolder2        
+exit 0
+
     #check if directory exists
         #but only if first run or previously created new folders
      getFolderStruct() {
